6b20dbfbe6fa2ccb4fdae20c83113afbecf74d20
jdelacruzjose <jdelacruzjose@gmail.com>
2021-06-08 22:46:19 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+window.onload = function () {
+  console.log("Hello World");
+  let count = 0;
+
+  const value = document.querySelector("#value");
+  const btn = document.querySelectorAll(".btn");
+
+  console.log(btn);
+
+  btn.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const styles = e.currentTarget.classList;
+      if (styles.contains("decrease")) {
+        count--;
+      } else if (styles.contains("increase")) {
+        count++;
+      } else {
+        count = 0;
+      }
+
+      if (count > 0) {
+        value.style.color = "green";
+      }
+      if (count < 0) {
+        value.style.color = "red";
+      }
+      if (count === 0) {
+        value.style.color = "#222";
+      }
+
+      value.textContent = count;
+    });
+  });
+};
